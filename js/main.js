84505e4c0eac0e53c450fd9d674e6b861549adda
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-10-20 14:33:07 +0200

    refactor

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 // get username
 const name = prompt('Please, enter your name 🙏');
+
+// get surname
 const surname = prompt('Great!, Now enter your surname 👌');
+
+// get colour
 const colour = prompt('Ok! What is your favourite color?🎨');
 
+// dom actions
 document.getElementById('title').innerHTML = name + surname + colour + '21';
 // document.getElementById('title').innerHTML = `${name}${surname}${colour}21`;
