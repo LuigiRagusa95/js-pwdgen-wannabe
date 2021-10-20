ff421a24ca4c821ae80abe255e1034a0d62788a4
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-10-20 14:27:37 +0200

    add password generator

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// get username
+const name = prompt('Please, enter your name 🙏');
+const surname = prompt('Great!, Now enter your surname 👌');
+const colour = prompt('Ok! What is your favourite color?');
+
+document.getElementById('title').innerHTML = `${name}${surname}${colour}21`;
