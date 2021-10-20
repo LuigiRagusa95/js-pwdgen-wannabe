4ecc701ad120212088f2bcf26611fe1aa16c1b94
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-10-20 14:29:47 +0200

    add string concatenation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // get username
 const name = prompt('Please, enter your name 🙏');
 const surname = prompt('Great!, Now enter your surname 👌');
-const colour = prompt('Ok! What is your favourite color?');
+const colour = prompt('Ok! What is your favourite color?🎨');
 
-document.getElementById('title').innerHTML = `${name}${surname}${colour}21`;
+document.getElementById('title').innerHTML = name + surname + colour + '21';
+// document.getElementById('title').innerHTML = `${name}${surname}${colour}21`;
